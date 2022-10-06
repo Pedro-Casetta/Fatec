8a72b469a26b29db7393955b7f0bbabbf5c2a885
Pedro-Casetta <111102537+Pedro-Casetta@users.noreply.github.com>
2022-10-06 15:40:53 -0300

    Add files via upload

---
+++ Exercício 9/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+Console.WriteLine("");
+Console.WriteLine("Exercício 8");
+Console.WriteLine("");
+Console.Write("Escreva seu nome completo: ");
+string nome = Console.ReadLine();
+char[] pilha = new char[nome.Length];
+int topo = 0;
+int i = 0;
+
+while (i < nome.Length)
+{
+    while (i < nome.Length && nome[i] != ' ')
+    {
+        Insere(pilha, ref topo, nome[i]);
+        i += 1;
+    }
+    while (EstaVazia(ref topo) == false)
+    {
+        char letra = Remove(pilha, ref topo);
+        Console.Write(letra);
+    }
+    i += 1;
+    Console.Write(" ");
+}
+
+void Insere(char[] pilha, ref int topo, char n)
+{
+        pilha[topo] = n;
+        topo += 1;
+}
+
+char Remove(char[] pilha, ref int topo)
+{
+    topo -= 1;
+    return pilha[topo];
+}
+
+bool EstaVazia(ref int topo)
+{
+    if (topo == 0)
+        return true;
+    else
+        return false;
+}
